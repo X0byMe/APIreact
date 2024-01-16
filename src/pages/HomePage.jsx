8cbb5a1574c964918a9b0aa0566c65ee0dcec743
WebDev <webdev@MacBook-Pro-de-Ludwig.local>
2024-01-16 09:28:04 +0100

    creation du dossier et homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+const HomePage = (props) => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Hello World!</h1>
+            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, odio, accusantium hic ea incidunt culpa vel perferendis non totam aperiam voluptatem nihil atque blanditiis, necessitatibus dolores suscipit iure neque labore! Ut est ex aut excepturi, iste sint nam facere. Nostrum odio doloremque repellat sint iure commodi cumque, culpa cupiditate enim!</p>
+            <hr className="my-4" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quo dolor esse inventore deserunt explicabo reiciendis corrupti vel a tempora.</p>
+            <p className="lead">
+                <a href="#" className="btn btn-primary btn-lg">Learn More</a>
+            </p>
+        </div>
+    );
+}
+ 
+export default HomePage;
