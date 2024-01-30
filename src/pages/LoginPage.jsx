c85a3a19424ba66e4788b258cd0e478006b5cae8
WebDev <webdev@MacBook-Pro-de-Ludwig.local>
2024-01-30 11:03:19 +0100

    Authentification suite

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,75 @@
+import { useState } from 'react'
+import authAPI from '../services/authAPI';
+
 const LoginPage = (props) => {
+    //Ceci est un hook qui crée un state (une variable d'état) dans react, car je dois envoyer un objet à mon API pour vérifier l'id et mot de passe du user. Donc par défaut tu as 1 objet, avec 2 paramètres : username et password. 
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: ""
+    })
+
+    // Afficher une erreur là où y'en a une; si y'en a une
+    const[error, setError] = useState("")
+
+    // HandleChange permet la fonctionnalité d'écriture dans un champs et de l'enregister/le récupérer en temps réel dans UseState
+    const handleChange = (event) => {
+        const value = event.currentTarget.value
+        const name = event.currentTarget.name
+
+        //pour copier ... et avec une virgule on peut ajouter ou remplacer un élément dans sa copie
+
+        //SetCredentials permet de modifier la ligne 7 et 8. Avec les "..." on copie le contenus de ces lignes, et il y appliques les nouvelles valeurs saisies dans le champs de saisi de connexion grace au HandleChange. 
+        setCredentials({...credentials, [name]:value})
+
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try{
+            await authAPI.authenticate(credentials)
+            setError("")
+        }catch(error)
+        {
+            setError("Aucun compte ne possède cette adresse e-mail ou les iformations ne correspondent pas")
+        }
+    }
+
     return ( 
         <>
             <div className="row">
                 <div className="col-4 offset-4">
                     <h1>Connexion</h1>
-                    <form></form>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group my-3">
+                            <label htmlFor="username">Adresse E-mail</label>
+                            <input 
+                                type="email" 
+                                value={credentials.username}
+                                onChange={handleChange}
+                                placeholder='Adresse E-mail de connexion'
+                                id='username'
+                                name='username'
+                                className={"form-control " + (error && "is-invalid")}
+                            />
+                        { error && (
+                                <p className='invalid-feedback'>{error}</p>
+                        )}
+                        </div>
+                        <div className="form-group my-3">
+                            <label htmlFor="password">Mot de passe</label>
+                            <input 
+                            type="password" 
+                            value={credentials.password}
+                            onChange={handleChange}
+                            placeholder='Mot de passe'
+                            id='password'
+                            name='password'
+                            className='form-control' />
+                        </div>
+                        <div className="form-group my-3">
+                            <button className="btn btn-success">Connexion</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
--- src/services/authAPI.js
+++ src/services/authAPI.js
@@ -7,7 +7,7 @@ function authenticate(credentials)
             .post("http://apicourse.myepse.be/api/login_check", credentials)
             .then(response => response.data.token)
             .then(token => {
-                // mettre le token dans le localStorage
+                // mettre le token dans le localStorage pour éviter de devoir se reconnecter à chaque fois à l'API. 
                 window.localStorage.setItem("authToken", token)
                 // aouter à Axios pour chaque req, le bearer token
                 Axios.defaults.headers["Authorization"] = "Bearer " + token
@@ -15,17 +15,24 @@ function authenticate(credentials)
             })
 }
 
+/**
+ * Si tu te deconnectes toi-même, je ne vais plus garder le token dans le navigateur.
+ */
 function logout(){
     window.localStorage.removeItem("authToken")
     delete Axios.defaults.headers["Authorization"]
 }
 
+/**
+ * Lorsque tu rechargeras ta page, tu iras vers l'index. Je veux donc vérifier si j'ai un token dans le navigateur pour éviter de devoir te reconnecter.
+ */
 function setup(){
     // voir si on a un token
     const token = window.localStorage.getItem("authToken")
     if(token)
     {
         const jwtData = jwtDecode(token)
+        // je récupère d'un symfo php (l'API) dont la réponse est en secondes, que je transfert à un react en JS qui est en milliseconde.
         if((jwtData.exp * 1000) > new Date().getTime())
         {
             Axios.defaults.headers["Authorization"]="Bearer " + token
@@ -33,9 +40,27 @@ function setup(){
     }
 }
 
+/**
+ * Vérifier si on est déjà connecté, ou si un token est déjà existant
+ */
+function isAuthenticated() {
+    const token = window.localStorage.getItem("authToken")
+    if(token)
+    {
+        const jwtData = jwtDecode(token)
+        if((jwtData.exp * 1000) > new Date().getTime())
+        {
+            return true //ok il est authentifié
+        }
+        return false // il existe mais est expiré
+    }
+    return false // pas de token
+}
+
 
 export default {
     authenticate: authenticate,
     logout: logout,
-    setup: setup
+    setup: setup,
+    isAuthenticated:isAuthenticated
 }
